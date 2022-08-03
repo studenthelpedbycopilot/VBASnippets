1975eab06946f114fa52a23083898c909c37eab0
studenthelpedbycopilot <110437297+studenthelpedbycopilot@users.noreply.github.com>
2022-08-02 21:38:33 -0300

    Update Filenames _to_array.vb

--- Filenames _to_array.vb
+++ Filenames _to_array.vb
@@ -1,34 +1,31 @@
+'get files in folder and put in array   
 'put filenames from a directory in a string array
 'Colocar os nomes dos arquivos em um array
 'language: vba
 
-Sub ListarArquivosnapasta() 
+'esta criei para ensinar GitHubCopilot
+'created for GithubCopilot machine learning
+
+Sub filesToArray()
+    
+    Dim objFSO As Object
+    Dim objFolder As Object
+    Dim objFile As Object 
+    Dim arrFiles() As String 
+    Dim i As Integer 
  
-  Dim ObjScriptArquivo As Object 
-  Dim ObjPasta As Object 
-  Dim ObjArquivo As Object 
-  Dim strpathFiles() As String 
-  Dim i As Integer 
+    Set objFSO = CreateObject("Scripting.FileSystemObject") 
+  Set objFolder = objFSO.GetFolder("C:\Users\Public\Pictures) 
+    i = 0 
  
-  i = 0 
-   
-  Set ObjScriptArquivo = CreateObject("Scripting.FileSystemObject") 
+    For Each objFile In objFolder.Files 
+        ReDim Preserve arrFiles(0 To i) 
+        arrFiles(i) = objFile.Name 
+        i = i + 1 
+    Next objFile 
  
-  Set ObjPasta = ObjScriptArquivo.GetFolder("C:\Users\Public\Pictures")
-
-  Debug.Print ObjPasta.Path
-
-  For Each ObjArquivo In ObjPasta.Files
-
-    ReDim Preserve strpathFiles(0 To i)
-    strpathFiles(i) = ObjPasta.Path & "\" & ObjArquivo.Name
-
-    Cells(i + 1, 1).Value = ObjArquivo.Name
-    Cells(i + 1, 2).Value = strpathFiles(i)
-
-    i = i + 1
-
-  Next ObjArquivo
-
-End Sub
-'Language: vba
+    For Each strFile In arrFiles 
+        Debug.Print strFile 
+    Next strFile 
+ 
+End Sub 
